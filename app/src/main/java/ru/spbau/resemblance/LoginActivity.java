ae4f3bd15630847d824d7fa55513ee1bfbfdd88b
unknown <andy.gusev@list.ru>
2016-12-04 16:18:34 +0300

    MD5

--- app/src/main/java/ru/spbau/resemblance/LoginActivity.java
+++ app/src/main/java/ru/spbau/resemblance/LoginActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText nicknameField = null;
@@ -22,10 +27,26 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginClick(View v) {
+        String nickname = nicknameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String passwordHash = null;
+
+        //Getting the hash of the password.
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] hash = messageDigest.digest(password.getBytes());
+            passwordHash = new BigInteger(1, hash).toString();
+        } catch (NoSuchAlgorithmException e) {
+            Log.d("MD5", "MD5 Algorithm not found.");
+        }
+
+
+        //SendMessageModule.sendMessage();
+
         SharedPreferences prefs = getSharedPreferences(SettingsActivity.PREFERENCES, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(SettingsActivity.NICKNAME_PREF, nicknameField.getText().toString());
-        editor.putString(SettingsActivity.PASSWORD_PREF, passwordField.getText().toString());
+        editor.putString(SettingsActivity.NICKNAME_PREF, nickname);
+        editor.putString(SettingsActivity.PASSWORD_PREF, passwordHash);
         editor.putInt(SettingsActivity.RATING_PREF, 100500);
         editor.commit();
 
