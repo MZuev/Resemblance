a2be63895bcead284dfd83685ea20a290c9a6724
unknown <andy.gusev@list.ru>
2016-12-12 23:02:57 +0300

    IP

--- app/src/main/java/ru/spbau/resemblance/SendMessageModule.java
+++ app/src/main/java/ru/spbau/resemblance/SendMessageModule.java
@@ -16,7 +16,7 @@ public class SendMessageModule {
     final private static String LOG_TAG = "Messager log";
     final private static int maxCntToReconnect = 20;
 
-    final private static String serverIP = "192.168.1.180";
+    final private static String serverIP = "10.0.0.2";
     final private static int serverPort = 6662;
 
     private final static int sleepTime = 1000;
