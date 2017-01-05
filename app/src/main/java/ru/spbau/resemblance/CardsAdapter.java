ea8bd643f419b3e61d061320ac35820d8c38cbb2
unknown <andy.gusev@list.ru>
2017-01-05 21:57:38 +0200

    Gallery performance improved.

--- app/src/main/java/ru/spbau/resemblance/CardsAdapter.java
+++ app/src/main/java/ru/spbau/resemblance/CardsAdapter.java
@@ -1,5 +1,6 @@
 package ru.spbau.resemblance;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
@@ -9,18 +10,50 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class CardsAdapter extends BaseAdapter {
-    private final Context context;
+    private final Activity activity;
     private final ImageStorage.ImageWrapped[] cards;
     private final int cardSide;
     private final ImageView[] views;
 
-    public CardsAdapter(Context context, ImageStorage.ImageWrapped[] cards, int cardSide) {
-        this.context = context;
+    public CardsAdapter(final Activity activity, final ImageStorage.ImageWrapped[] cards, int cardSide) {
+        this.activity = activity;
         this.cards = cards;
         this.cardSide = cardSide;
         views = new ImageView[cards.length];
+
+        //First we fill the grid with placeholders
+        ImageView placeholder = new ImageView(activity);
+        placeholder.setImageResource(android.R.drawable.ic_menu_report_image);
+        Arrays.fill(views, placeholder);
+        //Then we replace them with real pictures
+        //This is made for performance improvements
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GridView.LayoutParams sizeLayoutParams = new GridView.LayoutParams(CardsAdapter.this.cardSide,
+                        CardsAdapter.this.cardSide);
+                for (int i = 0; i < cards.length; i++) {
+                    //Setting picture from file and its parameters
+                    try {
+                        views[i] = cards[i].getPreview(CardsAdapter.this.activity, CardsAdapter.this.cardSide);
+                        views[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
+                        views[i].setLayoutParams(sizeLayoutParams);
+                    } catch (FileNotFoundException e) {
+                        Log.d("CardsAdapter", "File not found: " + cards[i].getUriImage());
+                    }
+                    //Updating screen
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            CardsAdapter.this.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -40,17 +73,6 @@ public class CardsAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (views[position] == null) {
-            try {
-                views[position] = cards[position].getPreview(context, cardSide);
-            } catch (FileNotFoundException e) {
-                views[position] = new ImageView(context);
-                views[position].setImageResource(R.drawable.empty);
-            }
-            views[position].setScaleType(ImageView.ScaleType.CENTER_CROP);
-            views[position].setLayoutParams(new GridView.LayoutParams(cardSide, cardSide));
-        }
-
         return views[position];
     }
 }
